27567e574a5d181b745fcb5d0b48d7676c6491cc
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-12-28 22:28:25 -0600

    Finished event.js

--- event.js
+++ event.js
@@ -1,8 +1,22 @@
-var events = require('events');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
-var emitter = new events.EventEmitter();
-emitter.on('customEvent', function(message, status) {
-  console.log(`${status}: ${message}`);
+// var emitter = new events.EventEmitter();
+// emitter.on('customEvent', function(message, status) {
+//   console.log(`${status}: ${message}`);
+// });
+
+// emitter.emit('customEvent', "Hello World", 200);
+
+var Person = function(name) {
+  this.name = name;
+};
+
+util.inherits(Person, EventEmitter);
+var marx = new Person("Karl Marx");
+
+marx.on('speak', function(said) {
+  console.log(`${this.name}: ${said}`);
 });
 
-emitter.emit('customEvent', "Hello World", 200);
+marx.emit('speak', "The history of all hitherto existing society is the history of class struggles.");
