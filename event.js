fb0215c5e0ab1d2e0124f0cd3921c89449b8b254
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-12-30 22:47:05 -0600

    New files

--- event.js
+++ event.js
@@ -1,5 +1,7 @@
-var EventEmitter = require('events').EventEmitter;
-var util = require('util');
+var Person = require('./lib/person');
+
+// var EventEmitter = require('events').EventEmitter;
+// var util = require('util');
 
 // var emitter = new events.EventEmitter();
 // emitter.on('customEvent', function(message, status) {
@@ -8,15 +10,21 @@ var util = require('util');
 
 // emitter.emit('customEvent', "Hello World", 200);
 
-var Person = function(name) {
-  this.name = name;
-};
+// var Person = function(name) {
+//   this.name = name;
+// };
 
-util.inherits(Person, EventEmitter);
+// util.inherits(Person, EventEmitter);
 var marx = new Person("Karl Marx");
+var george = new Person("George Washington");
 
 marx.on('speak', function(said) {
   console.log(`${this.name}: ${said}`);
 });
 
+george.on('speak', function(said) {
+  console.log(`${this.name} -> ${said}`);
+});
+
 marx.emit('speak', "The history of all hitherto existing society is the history of class struggles.");
+george.emit('speak', "It is far better to be alone than to be in bad company.");
